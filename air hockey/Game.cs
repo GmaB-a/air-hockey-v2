0fb8c1afb398ff642ba165cba20f08dfb743a40d
GmaB-a <vladik11012006@gmail.com>
2022-05-28 16:37:04 +0300

    bug fixed

--- air hockey/Game.cs
+++ air hockey/Game.cs
@@ -163,8 +163,8 @@ namespace air_hockey
 
         private bool IsOnRightX(Player player, int operator_func)
         {
-            if(operator_func == 1) return player.line.Position.X > ball.Position.X + ball.Radius;
-            return player.line.Position.X < ball.Position.X + ball.Radius;
+            if(operator_func == 1) return (player.line.Position.X < ball.Position.X + ball.Radius) && (player.line.Position.X > ball.Position.X);//(player.line.Position.X > ball.Position.X + ball.Radius) && (player.line.Position.X < ball.Position.X);
+            return (player.line.Position.X < ball.Position.X + ball.Radius) && (player.line.Position.X > ball.Position.X);
         }
 
         private void ChangeDirectionAndLastStriked(Player player)
